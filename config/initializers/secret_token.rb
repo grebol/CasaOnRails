054ed871e503fffd5d4d59cf62e41b5e70079d6c
Joao Gabriel A. Caribe <grebol171@gmail.com>
2013-02-25 01:46:10 -0300

    Primeira versão(não funcional)
    Nada interessante aqui

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CasaOnRails::Application.config.secret_token = '83ed364cb9a94487568d23ab3bd8ff4909c380abe5a3fa0fb636ed2e8d2601ccd56c96fe6e507fd2df854a7ce13d1f414b1745da5ed6e655fc9a5c7ea81508b5'
